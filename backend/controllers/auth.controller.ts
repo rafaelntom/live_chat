9a66393271038f1775f3a1f42b2fae0032c1e24d
RafaelTomazini <rafaelnt83@gmail.com>
2024-06-26 15:05:23 -0300

    mongoose connection

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 
 export const login = async (req: Request, res: Response) => {
-  return res.status(200).json({ message: "Login route" });
+  return res.status(200).json({ message: "login route" });
 };
 
 export const signup = async (req: Request, res: Response) => {
-  return res.status(200).json({ message: "Login route" });
+  return res.status(200).json({ message: "signup route" });
 };
 
 export const logout = async (req: Request, res: Response) => {
-  return res.status(200).json({ message: "Login route" });
+  return res.status(200).json({ message: "logout route" });
 };
--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import authRouter from "./routes/auth.routes";
+import { mongooseDBConnection } from "./db/mongoDBconnection";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use("/api/auth", authRouter);
 
-app.listen(port, () => {
-  console.log(` 🚀 Server is running on port ${port} 🚀`);
+app.listen(port, async () => {
+  await mongooseDBConnection().then(() => {
+    console.log(` 🚀 Server is running on port ${port} 🚀`);
+  });
 });
