3eb12f9ad9dc7713f0a52b8980eedbc50b6993d8
RafaelTomazini <rafaelnt83@gmail.com>
2024-07-18 09:34:19 -0300

    api endpoit to live deploy url

--- frontend/src/utils/axiosInstace.ts
+++ frontend/src/utils/axiosInstace.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://live-chat-wzeb.onrender.com",
+  baseURL: "https://live-chat-wzeb.onrender.com/api",
   withCredentials: true,
 });
 
